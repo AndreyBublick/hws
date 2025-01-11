008102253e78cff734e047aa900ee56a214100e5
Andrew <dracon.24041230@mail.ru>
2025-01-11 21:50:46 +0300

    hw7 + fixTest10

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -62,7 +62,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       {...restProps}
                   />
                   <span
-                      id={id + '-span-' + o.id}
+                      id={id + '-span-' + o.value}
                       {...spanProps}
                       className={spanClassName}
                   >
