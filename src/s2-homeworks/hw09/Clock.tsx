3871fff9c0ae3a98e75b3c101d14cb554258e167
Andrew <dracon.24041230@mail.ru>
2025-01-27 22:06:29 +0300

    hw9 + fix

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -47,11 +47,11 @@ function Clock() {
     const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
 
 
-    const stringDate = `${String(date.getDay() + 1).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = `${date.toLocaleString('en-US', { weekday: 'long' })}` || <br/> // пишут студенты
-    const stringMonth = `${date.toLocaleString('en-US', { month: "short" })}` || <br/> // пишут студенты
+    const stringMonth = `${date.toLocaleString('en-US', { month: "long" })}` || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
